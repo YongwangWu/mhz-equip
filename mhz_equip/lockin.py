7a8a4a347c76c950edf44520e99444ccbea1eab8
Zach Mitchell <zmitchell@fastmail.com>
2018-04-21 14:29:39 -0400

    Enable serial connections with the LIA
    This commit allows the lock-in amplifier to connect to a serial device.
    
    The test procedures for serial communication require a little bit of
    explanation. It would not be ideal to require a physical serial device
    for testing, so virtual serial devices are created with the `socat`
    utility. The devices created by `socat` are not guaranteed to have the
    same name each time, so an option is passed to `socat` to create
    symlinks at fixed locations.
    
    `socat` needs to be running the whole time the virtual devices need to
    exist, and it needs to run in the background. To do this, `socat` is run
    in a subprocess. The subprocess is terminated in the cleanup code for
    the `socat` fixture. There is a `time.sleep(0.5)` after starting the
    subprocess because `socat` takes a small amount of time to create the
    devices once it starts. Tests fail without the `sleep` because the
    devices haven't had enough time to be created.
    
    It would be ideal to setup and teardown a fresh set of ports for each
    test, but I'm looking ahead to tests that will use `hypothesis`.
    `hypothesis` will run a test several times with different inputs, but
    will only run the test fixtures once. Anything left in the device
    buffers would remain for the next test and contaminate the test
    environment. For these reasons `socat` is only started once, and each
    test will need to clear the device buffers.

--- mhz_equip/lockin.py
+++ mhz_equip/lockin.py
@@ -1,14 +1,37 @@
 import attr
+import logzero
+import trio
 
-from .util import Instrument
+from serial import Serial, SerialException
+from .util import Instrument, SerialConnectionError
 
 
 @attr.s
 class LockInAmplifier(Instrument):
-    current_cmd = attr.ib(default=None)
+
+    """The interface for the lock-in amplifier.
+
+    The lock-in amplifier (LIA) digitizes the signal from the detector and picks out
+    the component at the same frequency and at a fixed phase relative to the signal
+    provided at the Ref. input.
+    """
+
+    _port_settings = attr.ib()
+    _logger = attr.ib(default=logzero.logger)
+    _ser = attr.ib(init=False, default=None)
+    _current_cmd = attr.ib(init=False, default=None)
 
     async def connect(self, port_settings=None):
-        pass
+        settings = port_settings if port_settings is not None else self._port_settings
+        try:
+            ser = Serial(**settings)
+        except (SerialException, ValueError) as e:
+            self._logger.error("Could not connect to LIA port", exc_info=True)
+            raise SerialConnectionError
+
+        else:
+            self._ser = ser
+        self._aser = trio.wrap_file(self._ser)
 
     async def is_connected(self):
         pass
@@ -62,4 +85,25 @@ class LockInAmplifier(Instrument):
         pass
 
     async def is_overloaded(self):
+        pass
+
+    async def clear_status_bytes(self):
+        pass
+
+    async def set_data_channels(self):
+        pass
+
+    async def set_ref_source(self):
+        pass
+
+    async def set_input_mode(self):
+        pass
+
+    async def set_input_coupling(self):
+        pass
+
+    async def set_voltage_input_mode(self):
+        pass
+
+    async def set_grounding_mode(self):
         pass
--- tests/lockin_test.py
+++ tests/lockin_test.py
@@ -1,5 +1,64 @@
+import subprocess
+import time
+
+import pytest
+import trio
+
+from serial import Serial
 from mhz_equip.lockin import LockInAmplifier
 
 
+@pytest.fixture(scope="session")
+def socat():
+    port1 = "/tmp/port1"
+    port2 = "/tmp/port2"
+    args = ["socat", f"pty,rawer,echo=0,link={port1}", f"pty,rawer,echo=0,link={port2}"]
+    proc = subprocess.Popen(args)
+    time.sleep(0.5)  # wait for socat to create the ports in the separate process
+    yield (port1, port2)
+
+    proc.terminate()
+    proc.wait()
+
+
+@pytest.fixture
+def config_port1(socat):
+    config = {"port": socat[0], "baudrate": "115200"}
+    return config
+
+
+@pytest.fixture
+def config_port2(socat):
+    config = {"port": socat[1], "baudrate": "115200"}
+    return config
+
+
 def test_can_instantiate():
-    LockInAmplifier()
+    LockInAmplifier(port_settings={})
+
+
+def test_can_connect_to_port(config_port1):
+    lia = LockInAmplifier(port_settings=config_port1)
+    trio.run(lia.connect)
+    assert lia._ser is not None
+
+
+@pytest.mark.trio
+async def test_can_write_to_port(config_port1):
+    lia = LockInAmplifier(port_settings=config_port1)
+    await lia.connect()
+    lia._ser.reset_input_buffer()
+    lia._ser.reset_output_buffer()
+    await lia._aser.write(b"foo")
+
+
+@pytest.mark.trio
+async def test_can_read_from_port(config_port1, config_port2):
+    dummy_ser = Serial(**config_port2)
+    lia = LockInAmplifier(port_settings=config_port1)
+    await lia.connect()
+    lia._ser.reset_input_buffer()
+    lia._ser.reset_output_buffer()
+    dummy_ser.write(b"foo")
+    msg = await lia._aser.read(3)
+    assert msg == b"foo", f"Read {msg}, expected `foo`"
